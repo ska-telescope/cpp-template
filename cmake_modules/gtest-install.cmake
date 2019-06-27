715d752351d6b14db22adff33c9ceefdf13bff94
Stephen Ord <steve.ord@gmail.com>
2019-06-28 00:22:03 +1000

    Added GoogleTest and the first test and a test build stage

--- cmake_modules/gtest-install.cmake
+++ cmake_modules/gtest-install.cmake
@@ -1,6 +1,5 @@
-if(USE_GTEST)
-    include(ExternalProject)
-    ExternalProject_Add(gtest
+include(ExternalProject)
+ExternalProject_Add(gtest-install
         URL https://github.com/google/googletest/archive/release-1.8.1.zip
         URL_HASH SHA1=7b41ea3682937069e3ce32cb06619fead505795e
  	SOURCE_DIR        "${CMAKE_BINARY_DIR}/gtest-src"
@@ -9,5 +8,4 @@ if(USE_GTEST)
 	CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -S <SOURCE_DIR> -B <BINARY_DIR>
         BUILD_COMMAND make
         INSTALL_COMMAND make install
-    )
-endif(USE_GTEST)
+)
