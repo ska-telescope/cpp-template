b4cb70307ef806a173ab040d33dd9a97c84ca0cd
Stephen Ord <steve.ord@gmail.com>
2019-06-28 00:36:41 +1000

    fixed the cmake for the googletest .... i think

--- cmake_modules/gtest-install.cmake
+++ cmake_modules/gtest-install.cmake
@@ -3,9 +3,8 @@ ExternalProject_Add(gtest-install
         URL https://github.com/google/googletest/archive/release-1.8.1.zip
         URL_HASH SHA1=7b41ea3682937069e3ce32cb06619fead505795e
  	SOURCE_DIR        "${CMAKE_BINARY_DIR}/gtest-src"
-        BINARY_DIR        "${CMAKE_BINARY_DIR}/gtest-build"
 	INSTALL_DIR       "${CMAKE_BINARY_DIR}/gtest"
-	CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -S <SOURCE_DIR> -B <BINARY_DIR>
+	CONFIGURE_COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR> -S <SOURCE_DIR> 
         BUILD_COMMAND make
         INSTALL_COMMAND make install
 )
