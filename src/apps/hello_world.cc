de515304ef02c8a625285dd27ef60b45be001736
Rodrigo Tobar <rtobar@icrar.org>
2019-07-15 14:17:34 +0800

    Minor improvements to hello_world app
    Using EXIT_SUCCESS requires including cstdlib, so let's simply return 0
    instead (caught by iwyu). Improved spacing and indentation is included
    in this patch too.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/apps/hello_world.cc
+++ src/apps/hello_world.cc
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+
 #include "HelloWorldConfig.h"
 #include "top/nested/hello.h"
 #include "top/nested/wave.h"
@@ -8,10 +9,10 @@ int main() {
 	top::nested::wave theHello;
 
 	std::cout << "Hello world version: " << HelloWorld_VERSION_MAJOR << ":"
-		  << HelloWorld_VERSION_MINOR << ":"
+	          << HelloWorld_VERSION_MINOR << ":"
 	          << HelloWorld_VERSION_PATCH <<  std::endl;
 
 	theHello.greeting();
 
-	return EXIT_SUCCESS;
+	return 0;
 }
